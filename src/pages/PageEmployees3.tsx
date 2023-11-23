a2e5ae7f4aada07525a45cc070f06e493e6fa38c
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-23 01:45:16 +0100

    code in no errors

--- src/pages/PageEmployees3.tsx
+++ src/pages/PageEmployees3.tsx
@@ -1,6 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { IEmployee } from "../interfaces";
+import * as appModel from "../appModel";
+
 export const PageEmployees3 = () => {
+	const employeesQuery = useQuery<IEmployee[]>({
+		queryKey: ["employees"],
+		queryFn: async () => appModel.getEmployees(),
+	});
+
+	if (employeesQuery.isLoading) return <p>loading...</p>;
+	if (employeesQuery.isError)
+		return <pre>{JSON.stringify(employeesQuery.error)}</pre>;
 
 	return (
-		<p>emp3</p>
+		<>
+			{employeesQuery.data && (
+				<>
+					<h2 className="mb-3 text-2xl">
+						There are {employeesQuery.data.length} employees loaded
+						with React Query.
+					</h2>
+					<ul className="list-disc ml-5">
+						{employeesQuery.data.map((employee) => {
+							return (
+								<li key={employee.employeeID}>
+									{employee.lastName}, {employee.firstName}{" "}
+								</li>
+							);
+						})}
+					</ul>
+				</>
+			)}
+		</>
 	);
 };
